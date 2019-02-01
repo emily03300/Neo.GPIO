aabaf26773075f663f98117a1df08aa3ab9a42f3
EMILY <ley03300@gmail.com>
2019-01-31 16:09:00 -0800

    gpioexample_re

--- GpioExample.py
+++ GpioExample.py
@@ -10,13 +10,9 @@ neo = Gpio()  # create new Neo object
 
 pinTwo = 25  # pin to use
 pinThree = 3
-pinFour = 26  # pin to use
-pinFive = 3
 
 neo.pinMode(pinTwo, neo.OUTPUT)  # Use innerbank pin 2 and set it as output either 0 (neo.INPUT) or 1 (neo.OUTPUT)
 neo.pinMode(pinThree, neo.INPUT)  # Use pin three(innerbank) and read set state to read
-neo.pinMode(pinFour, neo.OUTPUT)  # Use innerbank pin 2 and set it as output either 0 (neo.INPUT) or 1 (neo.OUTPUT)
-neo.pinMode(pinFive, neo.INPUT)
 
 # Blink example
 for a in range(0, 10):  # Do for five times
@@ -24,11 +20,6 @@ for a in range(0, 10):  # Do for five times
     sleep(0.5)  # wait one second
     neo.digitalWrite(pinTwo, neo.LOW)  # write low value to pin
     sleep(0.5)  # wait one second
-    neo.digitalWrite(pinFour, neo.HIGH)  # write high value to pin
-    sleep(0.5)  # wait one second
-    neo.digitalWrite(pinFour, neo.LOW)  # write low value to pin
-    sleep(0.5)  # wait one second
-
 
 # Read pin
 print
