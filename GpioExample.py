d83caabab1b055e25e5d7abe596f426a11a6b4b4
EMILY <ley03300@gmail.com>
2019-02-13 15:25:54 -0800

    gpioexample_re

--- GpioExample.py
+++ GpioExample.py
@@ -146,13 +146,10 @@ from time import sleep  # import sleep to wait for blinks
 
 neo =Gpio()
 
-S0 = 8 # pin to use
-S1 = 9
-S2 = 10
-S3 = 11
-
-c0=(0,0,0,0)
-c1=[0,0,0,1]
+S0 = 24 # pin to use
+S1 = 25
+S2 = 26
+S3 = 27
 
 pinNum = [S0, S1, S2, S3]
 
@@ -163,10 +160,14 @@ for i in range(4):
     neo.pinMode(pinNum[i], neo.OUTPUT)
 
 neo.digitalWrite(pinNum[0], 1)
-sleep(0.5)
+# sleep(0.5)
 neo.digitalWrite(pinNum[1], 1)
-sleep(0.5)
+# sleep(0.5)
 neo.digitalWrite(pinNum[2], 1)
-sleep(0.5)
+# sleep(0.5)
 neo.digitalWrite(pinNum[3], 1)
-sleep(0.5)
+# sleep(0.5)
+
+raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
+scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
+print (raw * scale)
