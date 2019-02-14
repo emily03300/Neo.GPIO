5c097f6125a538bcb05fd775376b7bc416d9c1a9
EMILY <ley03300@gmail.com>
2019-02-14 00:42:42 -0800

    gpioexample_re

--- temp.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-while True:
-    raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-    scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-    v = raw * scale
-    t = (v - 500)/10 - 6
-    sleep(1)
-    print (t)
