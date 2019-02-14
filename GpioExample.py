ee13b40ab636bc838ff9d944238592662f4c5ce2
EMILY <ley03300@gmail.com>
2019-02-14 00:21:10 -0800

    gpioexample_re

--- GpioExample.py
+++ GpioExample.py
@@ -251,48 +251,25 @@
 # print(raw * scale)
 
 
-
-import threading
 from neo import Gpio
+from time import sleep
 
-class BTMUXcontrol:
-    def __init__(self):
-        self.temp = 0
-        self.output_pins = [24, 25, 26, 27]
-        self.IOLock = threading.Lock()  # mutual lock for R/W control
-        self.gpio = Gpio()              # initialize GPIO controller
+neo = Gpio()
 
-        # setup output pins
-        self.IOLock.acquire()
-        for pin in self.output_pins:
-            self.gpio.pinMode(pin, self.gpio.OUTPUT)
-            self.gpio.digitalWrite(pin, self.gpio.LOW)
-        self.IOLock.release()
+pinNum = [0,1,2,3]
 
-    def decToBin(self, n):
-        num = [0,0,0,0]
-        if n == 0:
-            return num
-        else:
-            for x in range(n):
-                t = x + 1
-                for y in range(4):
-                    num[y] = t%2
-                    t = t/2
-            return num
+for i in pinNum:
+    neo.pinMode(pinNum[i], neo.OUTPUT)
 
-    def mux_channel(self, channel):
-        bin_list = self.decToBin(channel) #binary list
-        self.IOLock.acquire()
-        for z in range(4):
-            self.gpio.digitalWrite(self.output_pins[z],bin_list[z])
-        self.IOLock.release()
-        #return True
+for x in range(15):
+    num = [0,0,0,0]
+    t = x + 1
+    for y in range(4):
+        num[y] = t%2
+        t = t/2
 
-    def read_ADC(self):
-        self.IOLock.acquire()
-        raw = int(open("/sys/bus/iio/devices/iio:device0/in_voltage0_raw").read())
-        scale = float(open("/sys/bus/iio/devices/iio:device0/in_voltage_scale").read())
-        vout = raw * scale
-        self.IOLock.release()
-        return vout
+    neo.digitalWrite(pinNum[3], num[3])
+    neo.digitalWrite(pinNum[2], num[2])
+    neo.digitalWrite(pinNum[1], num[1])
+    neo.digitalWrite(pinNum[0], num[0])
+    sleep(1)
